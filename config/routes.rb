6990dc3ecc6f4b2568f8fc1813cd06d1c4122624
rjmmoore Moore <rob.moore@kellogg.northwestern.edu>
2025-02-22 23:26:24 +0000

    Completed website - to test more tomorrow

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  get 'entries/new'
-  get 'entries/create'
-  get 'places/index'
-  get 'places/show'
-  get 'places/new'
-  get 'places/create'
+  # get 'entries/new'
+  # get 'entries/create'
+  # get 'places/index'
+  # get 'places/show'
+  # get 'places/new'
+  # get 'places/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
   
 # Routes for Places
-get("/places", { :controller => "places", :action => "index" })   # List all places
-get("/places/new", { :controller => "places", :action => "new" }) # Form to add a new place
-post("/places", { :controller => "places", :action => "create" }) # Create a new place
-get("/places/:id", { :controller => "places", :action => "show" }) # Show a single place
+get("/places", { :controller => "places", :action => "index" }) 
+get("/places/new", { :controller => "places", :action => "new" })
+post("/places", { :controller => "places", :action => "create" }) 
+get("/places/:id", { :controller => "places", :action => "show" }) 
 
-# Routes for Entries (nested under places)
-get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" }) # Form to add a journal entry for a place
-post("/places/:place_id/entries", { :controller => "entries", :action => "create" }) # Save a new entry for a place
+# Routes for Entries
+get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" })
+post("/places/:place_id/entries", { :controller => "entries", :action => "create" })
 
 # Root route (homepage)
 get("/", { :controller => "places", :action => "index" }) # Homepage shows list of places
